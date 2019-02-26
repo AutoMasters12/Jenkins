c9f392880ab5df3e38ff7d35a89cec2bba358c6a
Automasters12 <suman143learning@gmail.com>
2019-02-26 19:47:57 +0530

    first project

---
+++ BBB/src/test/java/tests/TestSuite2.java
@@ -0,0 +1,26 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestSuite2 {
+
+	@Test
+	public void test1() {
+		System.out.println("test1");
+	}
+	@Test
+	public void test2() {
+		System.out.println("test2");
+	}
+	@Test
+	public void test3() {
+		System.out.println("test3");
+	}
+	@Test
+	public void test4() {
+		System.out.println("test4");
+	}
+
+}
